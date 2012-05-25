7cc8282c30ebcf8be088ef29090b7076593d2137
Alex Brandt <alunduil@alunduil.com>
2012-05-25 15:21:17 -0500

    Basic daemonization added.

--- pmort/__init__.py
+++ pmort/__init__.py
@@ -16,5 +16,3 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place - Suite 330, Boston, MA  02111-1307, USA.
 
-"""The program module for nebulamp. """
-
--- pmort/application.py
+++ pmort/application.py
@@ -19,6 +19,12 @@
 import argparse
 import datetime
 import sys
+import daemon
+import lockfile
+import signal
+import pwd
+import grp
+import os
 
 import pmort.helpers as helpers
 
@@ -41,43 +47,84 @@ class PostMortemApplication(object): #pylint: disable-msg=R0903
         # Other option handling ...
         helpers.COLORIZE = self.arguments.color
 
-    def run(self):
+        if self.arguments.debug:
+            helpers.debug({
+                "self.arguments.daemonize":self.arguments.daemonize,
+                })
+
+        if self.arguments.daemonize:
+            self.run = self.daemonize
+        else:
+            self.run = self.iteration
+
+    def iteration(self):
         verbosity = {
                 "verbose": self.arguments.verbose,
                 "debug": self.arguments.debug,
                 }
 
-        account = Account.login(
-                username = self.arguments.username,
-                password = self.arguments.password,
-                provider = self.arguments.cloud
+        if os.getloadavg()[1] > self.load_threshold:
+            self.parallel_iteration(verbosity)
+        else:
+            self.serial_iteration(verbosity)
+
+    def serial_iteration(self, verbosity):
+        for plugin in PostMortemPlugins(**verbosity):
+            self._single_iteration()
+
+    def _single_iteration(self, plugin):
+        output = plugin.run()
+        self.log(plugin, output)
+
+    def parallel_iteration(self, verbosity):
+        for plugin in PostMortemPlugins(**verbosity):
+            thread = threading.Thread(target = self._single_iteration, name = plugin, args = (self, plugin))
+            thread.start()
+
+        if self.arguments.debug:
+            helpers.debug({
+                "threading.active_count()":threading.active_count(),
+                "threading.enumerate()":threading.enumerate(),
+                })
+
+    def daemonize(self):
+
+        context = daemon.DaemonContext(
+                working_directory = '/var/log/psmort',
+                umask = 0o002,
+                pidfile = lockfile.FileLock(self.arguments.pidfile),
                 )
 
-        previous = Storage(**verbosity)
+        def stop():
+            pass
 
-        if self.arguments.debug:
-            helpers.debug({
-                "servers":account.servers,
-                })
+        def reinitialize():
+            pass
 
-        add = list(set(account.servers) - set(previous.servers))
-        remove = list(set(previous.servers) - set(account.servers))
+        def iteration():
+            return self.iteration()
 
-        if self.arguments.debug:
-            helpers.debug({
-                "add":add,
-                "remove":remove,
-                })
+        context.signal_map = {
+                signal.SIGTERM: stop,
+                signal.SIGHUP: reinitialize,
+                signal.SIGINT: 'terminate',
+                signal.SIGALRM: iteration,
+                }
 
-        verbosity["plugin_directory"] = self.arguments.plugins
+        if self.arguments.chroot:
+            context.chroot = self.arguments.chroot
 
-        for plugin in PostMortemPlugins(**verbosity):
-            for server in add:
-                plugin.add(server)
-            for server in remove:
-                plugin.remove(server)
+        context.uid = pwd.getpwnam(self.arguments.uid).pw_uid
+        context.gid = grp.getgrnam(self.arguments.gid).gr_gid
 
-        previous.servers = account.servers
+        with context:
+            self.schedule()
+            signal.pause()
+
+    def schedule(self):
+        load_ratio = os.getloadavg()[0]/self.load_threshold
+        sleep_time = (1.0 - load_ratio)*self.arguments.poll_seconds + load_ratio
+        signal.alarm(sleep_time)
 
 class PostMortemOptions(object):
     def __init__(self, name):
@@ -128,6 +175,60 @@ class PostMortemOptions(object):
                 ]
         self.parser.add_argument("--color", 
                 choices = ["none", "light", "dark", "auto"], default = "auto",
+                help = "".join(help_list))
+
+        # --daemonize, -d
+        help_list = [
+                "Specifies that the application should run as a daemon rather ",
+                "than just one run of each plugin.",
+                ]
+        self.parser.add_argument("--daemonize", "-d", action = "store_true",
+                default = False, help = "".join(help_list))
+
+        # --poll_seconds, -s
+        help_list = [
+                "Specifies the polling period for subsequent runs of the ",
+                "plugged in monitors.  The time, S, specified is assumed to ",
+                "be seconds.",
+                ]
+        self.parser.add_argument("--poll_seconds", "-s", type = int,
+                metavar = "S", help = "".join(help_list))
+
+        # --pidfile, -p
+        help_list = [
+                "Specified the location of the process id file.  Defaults to ",
+                "/var/run/pmort.pid.",
+                ]
+        self.parser.add_argument("--pidfile", "-p", 
+                default = "/var/run/pmort.pid", help = "".join(help_list))
+
+        # --configfile, -c
+        help_list = [
+                "Specifies the location of the configuration file.  Defaults ",
+                "to /etc/pmort/pmort.conf.",
+                ]
+        self.parser.add_argument("--configfile", "-c",
+                default = "/etc/pmort/pmort.conf", help = "".join(help_list))
+
+        # --chroot, -r
+        help_list = [
+                "Specifies a directoy to chroot into after starting.",
+                ]
+        self.parser.add_argument("--chroot", "-r", default = None,
+                help = "".join(help_list))
+
+        # --uid, -u
+        help_list = [
+                "Specifies the username the daemon should run as.",
+                ]
+        self.parser.add_argument("--uid", "-u", default = "pmort",
+                help = "".join(help_list))
+
+        # --gid, -g
+        help_list = [
+                "Specifies the groupname the daemon should run as.",
+                ]
+        self.parser.add_argument("--gid", "-g", default = "pmort",
                 help = "".join(help_list))
 
         return self._parser
